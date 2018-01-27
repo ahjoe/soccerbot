6eb6e392a10ef5a53aade4dc9937238233f53628
ahjoe <ahjoe@twopings.com>
2018-01-27 18:25:50 +0800

    added test case

---
+++ tests/urltest.py
@@ -0,0 +1,14 @@
+""" Test for url request & response handlers via python v3 builtin modules
+
+This test demostrates how to read json response through url path
+
+Example:
+    $ python urltest.py 'http://www.google.com'
+
+Attributes:
+
+TODO:
+    * TODO #1
+    * TODO #2
+
+"""
