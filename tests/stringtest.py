7669468c7bd9be570bfa27b4d39261c18692f683
ahjoe <ahjoe@twopings.com>
2018-01-28 19:27:05 +0800

    add logging and more case

--- tests/stringtest.py
+++ tests/stringtest.py
@@ -8,19 +8,53 @@ Example:
 Attributes:
 
 """
+import os
 import unittest
+import logging
 
 #
-# TODO:
-# FIXME:
+# TODO: todo example here
+# FIXME: fixme example here
 #
+
+# ====================
+# logger configuration
+# ====================
+# create logger
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+
+# create formatter
+formatter = logging.Formatter(
+    '[%(asctime)s.%(msecs)03d] %(name)s %(levelname)-8s: %(message)s'
+    , '%Y-%m-%d %H:%M:%S'
+    )
+
+# create console handler and append into logger
+ch = logging.StreamHandler()
+ch.setLevel(logging.DEBUG)
+ch.setFormatter(formatter)
+logger.addHandler(ch)
+
+# create file handler and append into logger
+LOG_RELATIVE_PATH = '/../log/soccerbot.log'
+fh = logging.FileHandler(
+    os.path.dirname(os.path.realpath(__file__))
+    + LOG_RELATIVE_PATH)
+fh.setLevel(logging.INFO)
+fh.setFormatter(formatter)
+logger.addHandler(fh)
+# =======================
+
 class TestStringMethods(unittest.TestCase):
     def test_upper(self):
         self.assertEqual('foo'.upper(), 'FOO')
 
     def test_isupper(self):
+        logger.info('start running test_isupper')
         self.assertTrue('FOO'.isupper())
         self.assertFalse('foo'.isupper())
+        logger.info('end running test_isupper')
 
     @unittest.skip("This test would be skipped")
     def test_skip(self):
--- tests/urltest.py
+++ tests/urltest.py
@@ -8,3 +8,55 @@ Example:
 Attributes:
 
 """
+
+import os
+import unittest
+import logging
+import urllib.request
+import json
+
+# ====================
+# logger configuration
+# ====================
+# create logger
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+
+# create formatter
+formatter = logging.Formatter(
+    '[%(asctime)s.%(msecs)03d] %(name)s %(levelname)-8s: %(message)s'
+    , '%Y-%m-%d %H:%M:%S'
+    )
+
+# create console handler and append into logger
+ch = logging.StreamHandler()
+ch.setLevel(logging.DEBUG)
+ch.setFormatter(formatter)
+logger.addHandler(ch)
+
+# create file handler and append into logger
+LOG_RELATIVE_PATH = '/../log/soccerbot.log'
+fh = logging.FileHandler(
+    os.path.dirname(os.path.realpath(__file__))
+    + LOG_RELATIVE_PATH)
+fh.setLevel(logging.INFO)
+fh.setFormatter(formatter)
+logger.addHandler(fh)
+# =======================
+
+class TestUrlJson(unittest.TestCase):
+    def test_readjson(self):
+        url = 'http://api.football-data.org/v1/teams/5'
+        req = urllib.request.Request(url=url, headers={}, method=None)
+        res = urllib.request.urlopen(req, timeout=5)
+        data = res.read()
+        encoding = res.info().get_content_charset('utf-8')
+        JSON = json.loads(data.decode(encoding))
+        logger.info(
+            json.dumps(JSON, indent=4, sort_keys=True)
+        )
+        self.assertTrue(JSON != None)
+
+
+if __name__ == '__main__':
+    unittest.main()
